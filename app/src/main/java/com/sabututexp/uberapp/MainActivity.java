d87950bd80db32c8c5a533a7a45e8f349404650b
sabu0050 <devilsworld27@gmail.com>
2017-10-26 00:20:49 +0600

    first commit

---
+++ app/src/main/java/com/sabututexp/uberapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.sabututexp.uberapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button dButton;
+    private Button cButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dButton = (Button) findViewById(R.id.driverButton);
+        cButton = (Button) findViewById(R.id.customerButton);
+
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,DriverRegistration.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+        cButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,CustomerRegistration.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+    }
+}
